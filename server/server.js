e77c8c580f0dc6600e87cbaece8207668c5f800f
Vixcy300 <vigneshvani2007@gmail.com>
2026-06-04 23:01:42 +0530

    chore: add uncaught exception handlers to debug Render crash

--- server/server.js
+++ server/server.js
@@ -1,4 +1,15 @@
 require('dotenv').config();
+
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('UNHANDLED REJECTION at:', promise, 'reason:', reason);
+  process.exit(1);
+});
+
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
